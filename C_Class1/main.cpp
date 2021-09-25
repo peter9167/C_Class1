4bacf2fd61d49b65ad87149042cbb26dfaef4db2
peter <peter9167@naver.com>
2021-09-26 01:54:55 +0900

    first commit

---
+++ C_Class1/main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/*
+		첫번째 작성한 C 프로그램입니다.
+		2021.05.04
+	*/
+	printf("Hello !  \n");
+	printf("I am Student. \n");
+	printf("%d\n", 1234);
+	printf("%d   %d \n ", 10, 20);
+	return 1;
+}
